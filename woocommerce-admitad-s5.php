04e8c4dd53638e20ee609c75621a0a0eb952ae7d
yumashev <a.yumashev@gmail.com>
2017-01-19 04:23:25 +0300

    0.2

--- woocommerce-admitad-s5.php
+++ woocommerce-admitad-s5.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: WooCommerce Admitad S5
-Version: 0.1
+Version: 0.2
 Plugin URI: ${TM_PLUGIN_BASE}
 Description: Connect Admitad CPA network for WooCommerce catalog
 Author: AY
@@ -32,8 +32,8 @@ class woo_admitad{
 
 
   function additional_mime_types( $mimes ) {
-    	$mimes['xml'] = 'text/xml';
-    	$mimes['tmp'] = 'text/tmp';
+    	$mimes['xml'] = 'application/xml';
+    	// $mimes['tmp'] = 'text/tmp';
 
     	return $mimes;
     }
@@ -59,29 +59,31 @@ class woo_admitad{
 
       //Timb https://www.admitad.com/ru/webmaster/websites/583212/offers/15006/#products
       $url = 'http://export.admitad.com/ru/webmaster/websites/583212/products/export_adv_products/?user=yumashev&code=5d6968c590&feed_id=15074&format=xml';
-      $file_id = $this->save_file_by_url($url);
 
-      var_dump($file_id); exit;
-
-      $this->work($file_id);
-
+      if($att_id = get_transient( 'woo_at_media_id' )){
+        $this->work($att_id);
+      } else {
+        $att_id = $this->save_file_by_url($url);
+        set_transient( 'woo_at_media_id', $att_id, HOUR_IN_SECONDS );
+        $this->work($att_id);
+      }
 
     }
 
 
-    function work($file_id){
+    function work($att_id){
 
-      var_dump($file_id); exit;
-
-      $file = 'get_path_file from $file_id';
+      $file = get_attached_file( $att_id );
+      printf('<p>Work with file: %s</p>', $file);
 
       $reader = new XMLReader;
-      $xml = $reader->open($file);
+      $reader->open($file);
+
 
       $i = 0;
-      while($xml->read()){
+      while($reader->read()){
 
-        var_dump($xml);
+        var_dump($reader->readString());
         echo '<hr>';
 
         if($i > 11){
@@ -118,22 +120,15 @@ class woo_admitad{
           // var_dump($file_name); exit;
           $file_data = array(
         		'name'     => $file_name,
-        		'type'     => 'text/xml',
+        		'type'     => 'application/xml',
         		'tmp_name' => $temp_file,
         		'error'    => 0,
         		'size'     => filesize($temp_file),
         	);
 
           $overrides = array(
-        		// скажем WP не искать поля формы, которые обычно должны быть. По умолчанию true
-        		// Мы загружаем файл с удаленного сервера, поэтому полей формы у нас нет...
         		'test_form' => false,
-
-        		// если установить true, то WP будет пропускать пустые файлы. Не рекомендуется.
-        		'test_size' => true,
-
-        		// Правильно загруженный файл пройдет эту проверку.
-        		// Поэтому не нужно изменять этот параметр.
+        		'test_size' => false,
         		'test_upload' => false,
         	);
 
